f65d1ecb341b45fbedca6403fb28993e56713077
chaitanya <ckulkarni120@gmail.com>
2020-09-20 23:29:06 +0530

    PORT changes

--- Server.py
+++ Server.py
@@ -1,10 +1,10 @@
-# import socket
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# sock.bind(('0.0.0.0', 0))
-# LISTEN_ADDRESS = ('0.0.0.0', sock.getsockname()[1])
-# print(LISTEN_ADDRESS)
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('0.0.0.0', 0))
+LISTEN_ADDRESS = ('0.0.0.0', sock.getsockname()[1])
+print(LISTEN_ADDRESS)
 import json
-LISTEN_ADDRESS = ('127.0.0.1', 9999)
+# LISTEN_ADDRESS = ('127.0.0.1', 9999)
 import websockets
 from Data_Handler import *
 start_server = websockets.serve(Data, *LISTEN_ADDRESS)
